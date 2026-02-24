8436d7e1343e828b28e530923086683e2a07de24
candelacabell0 <ccabelloromero2122@insgabrielamistral.cat>
2026-02-24 13:17:55 +0100

    4a modificació Git Branca2

--- Practica9GIT_HUB/src/candela/Main.java
+++ Practica9GIT_HUB/src/candela/Main.java
@@ -2,7 +2,7 @@ package candela;
 
 public class Main {
 	public static void main (String[] args) {
-		System.out.println("3a modificació de Git.");
+		System.out.println("4a modificacio de Git Branca 2");
 	}
 
 }
